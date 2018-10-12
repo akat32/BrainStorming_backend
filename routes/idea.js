7c1992a5f74499466cf44e8dd94cc248d6915b2b
akat32 <baduk32127@gmail.com>
2018-10-12 20:54:32 +0900

    add Docs

--- routes/idea.js
+++ routes/idea.js
@@ -9,7 +9,7 @@ function idea(app, Rooms) {
   .post('/delIdea', async(req,res)=>{
     var result = await Rooms.update({code: req.body.code}, {$pull: {ideaList: req.body.idea}})
     if(!result.ok) return res.status(500).json({message: "ERR!"})
-    else return res.status(200).json({message: "success!"})  
+    else return res.status(200).json({message: "success!"})
   })
   .post('/getIdea', async(req,res)=>{
     var result = await Rooms.findOne({code: req.body.code})
